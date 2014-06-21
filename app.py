feba42dfce0c93a489886bf6046f52da3ddaa828
jiho <polaris341@gmail.com>
2014-06-21 18:18:12 +0900

    minimal application

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, redirect
+
+
+
+#create app
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+
+
+#################### run server#######################################################
+if __name__ == '__main__' :
+	app.run(host='0.0.0.0')
