f0dbebb88bcadce69d54b70eaabdabed1fa5e231
jiho <polaris341@gmail.com>
2014-06-23 20:17:27 +0900

    add script to index.html

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,4 +1,7 @@
-var pusher = new Pusher('5c4043a6d9b4d7f1da8d');
-pusher.connection.bind('connected', function() {
-  alert("Aa");
+
+$(document).ready(function(){
+	var pusher = new Pusher('5c4043a6d9b4d7f1da8d');
+	pusher.connection.bind('connected', function() {
+		alert("Aa");
+	});
 });
